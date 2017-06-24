649e9f864da1e9270eb585c02edeb10eff4ebe90
munjaza <munjaza@gmail.com>
2017-06-24 19:34:39 +0200

    izmene u logovanju

--- src/main/java/wordcount2/WordCount2.java
+++ src/main/java/wordcount2/WordCount2.java
@@ -1,28 +1,64 @@
 package wordcount2;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WordCount2 {
+public class WordCount2 extends Configured implements Tool {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WordCount2.class);
+    private String[] remainingArgs;
+
+    public WordCount2(Configuration config) {
+        super();
+        setConf(config);
+    }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
-        String[] remainingArgs = optionParser.getRemainingArgs();
-        if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
-            System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
-            System.exit(2);
+        Configuration config = new Configuration();
+        WordCount2 mr = new WordCount2(config);
+        ToolRunner.run(config, mr, args);
+    }
+
+    protected boolean validateAndParseInput(String[] args) {
+
+        GenericOptionsParser optionParser;
+        try {
+            optionParser = new GenericOptionsParser(getConf(), args);
+            remainingArgs = optionParser.getRemainingArgs();
+            if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+                LOGGER.error("Usage: wordcount <in> <out> [-skip skipPatternFile]");
+                System.exit(2);
+            }
+        } catch (IOException e) {
+            LOGGER.error(e.getLocalizedMessage());
         }
-        Job job = Job.getInstance(conf, "word count");
+
+        LOGGER.info("Input validation succeeded");
+        return true;
+    }
+
+    public int run(String[] args) throws Exception {
+        if (!validateAndParseInput(args)) {
+            throw new RuntimeException("Losi ulazni parametri!");
+        }
+
+        Job job = Job.getInstance(getConf(), "word count");
         job.setJarByClass(WordCount2.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
@@ -40,8 +76,13 @@ public class WordCount2 {
             }
         }
         FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+        if(args[1].equals("output/")) {
+            FileSystem.get(getConf()).delete(new Path(args[1]), true);
+            TextOutputFormat.setOutputPath(job, new Path(args[1]));
+        }
+        //FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
+
 }
